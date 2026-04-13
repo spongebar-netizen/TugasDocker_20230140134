7026293ae72c68609c7c1a49007e783e0dae03d2
Lalu Fatan <lalufatan25@gmail.com>
2026-04-13 15:10:35 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+
+
+import com.example.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> userList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String loginPage(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("admin".equals(username) && "20230140134".equals(password)) {
+            return "redirect:/home";
+        }
+        model.addAttribute("error", "Username atau Password salah!");
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("users", userList);
+        return "home";
+    }
+
+    @GetMapping("/form")
+    public String formPage(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/submit")
+    public String submitData(@ModelAttribute User user) {
+        userList.add(user);
+        return "redirect:/home";
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
+}
